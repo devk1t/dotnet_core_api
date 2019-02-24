f61db3e46bb446378fbb56e97eb17465ce21ef59
Matthew Van Achteren [Kinetic IT] <Matthew.VanAchteren@kineticit.com.au>
2019-02-24 13:33:35 +1100

    init

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+    private readonly IHttpClientFactory _httpClientFactory;
+  
+    public ValuesController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+  
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        var client = _httpClientFactory.CreateClient("test");
+        string result = await client.GetStringAsync("/bins/ncun4");
+        return Ok(result);
+    }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
